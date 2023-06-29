a12a7852851b947de014a7ad5fb38f2d6fdebd58
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-29 23:05:27 +0530

    [Sachin] Adding Welcome Message

---
+++ Oopsconcept289/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Oopsconcept289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the oops concept program");
+        }
+    }
+}
