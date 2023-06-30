3d1194ea019ede59f489dd8a9bb513ebc38075c4
Sachinmahajana <sachinmahajan782@gmail.com>
2023-06-30 15:10:55 +0530

    [Sachin] Adding inheritance

--- Oopsconcept289/Program.cs
+++ Oopsconcept289/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the oops concept program");
+            Console.WriteLine("Please choose the proram given below");
+            Console.WriteLine("\n1:Singleinheritance");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Toyota car = new Toyota();
+                    car.name = "Toyota Glanza";
+                    car.model = "Glanza S Amt";
+                    car.price = 815000;
+                    car.mileage = 22.94f;
+                    car.CarDetails();
+                    car.PrintMileage();
+                    break;
+            }
         }
     }
 }
---
+++ Oopsconcept289/Singleinheritance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oopsconcept289
+{
+    public class Car
+    {
+        public string name;
+        public string model;
+        public int price;
+        public void CarDetails()
+        {
+            Console.WriteLine("Car Name is:" + name);
+            Console.WriteLine("Car Model is:" + model);
+            Console.WriteLine("Car Price is:" + price);
+        }
+    }
+    public class Toyota : Car
+    {
+        public float mileage;
+        public void PrintMileage()
+        {
+            Console.WriteLine("Car Mileage is:" + mileage);
+        }
+    }
+}
